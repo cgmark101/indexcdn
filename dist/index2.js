4c5bf2bcd7674a6f1e5b76cbc8dec8fff06beeaf
cgmark101 <cgmark101@gmail.com>
2021-01-27 15:55:36 -0400

    ad index2

---
+++ dist/index2.js
@@ -0,0 +1,16 @@
+var map = L.map('map'),
+
+    realtime = L.realtime('https://wac5gf.deta.dev/deta/', {
+        interval: 3 * 10000
+    }).addTo(map);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+realtime.on('update', function () {
+    map.fitBounds(realtime.getBounds(), {
+        maxZoom: 17
+    });
+});
+
